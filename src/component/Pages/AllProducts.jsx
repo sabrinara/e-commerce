3b81f70646922c81bf2ed39b73f682e70e18288e
sabrinara <saashid18@gmail.com>
2024-03-19 00:50:50 +0600

    rating added

--- src/component/Pages/AllProducts.jsx
+++ src/component/Pages/AllProducts.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react"; // Import useState
 import { Link } from "react-router-dom";
-
+import { FaStar } from "react-icons/fa";
 const AllProducts = () => {
     const [allProducts, setAllProducts] = useState([]); // Initialize allProducts with an empty array and use useState
 
@@ -10,9 +10,17 @@ const AllProducts = () => {
             .then(res => setAllProducts(res.data))
             .catch(err => console.log(err));
     }, []); 
+
+    const generateStars = (rating) => {
+        const stars = [];
+        for (let i = 0; i < rating; i++) {
+            stars.push(<FaStar key={i} className="text-yellow-500 inline" />);
+        }
+        return stars;
+    };
     return (
         <div className="py-10 bg-emerald-100">
-            <div className="grid grid-cols-1 md:grid-cols-3  gap-10 m-10 md:m-14 lg:m-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10 m-10 md:m-14 lg:m-16">
                 {allProducts.map((product, index) => (
                     <div key={product.id} data-aos={index % 2 === 0 ? 'fade-up' : 'fade-down'}>
                         
@@ -20,10 +28,17 @@ const AllProducts = () => {
                             <figure><img src={product.img} className="w-full h-96 md:h-64 lg:h-full lg:object-center" alt="Product" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{product.name}</h2>
-                                <div className="flex justify-between items-center gap-36">
+                                <div className="flex justify-between items-center gap-28">
                                 <p>{product.category}</p>
                                 <p className="text-cyan-900 font-bold justify-end"> <span className="text-black">{product.seller}</span></p>
                                 </div>
+                                <div className="flex justify-between items-center gap-32">
+                                    
+                                     <p>Price: <span className="text-red-600">{product.price}$</span></p>
+                                     <p className="">{generateStars(product.ratings)}</p> 
+                                </div>
+                               
+                               
                                 <div className="card-actions">
                                     <div className="flex justify-between gap-28">
                                     <button className="bg-cyan-700 text-white py-2 px-4 rounded">Add to Cart</button>
