8523953d9cc00fc72f70070f248e9f67f8b5d015
sabrinara <saashid18@gmail.com>
2024-03-20 02:32:32 +0600

    customer list

---
+++ src/component/Pages/Dashboard/CustomerList.jsx
@@ -0,0 +1,17 @@
+
+
+const CustomerList = () => {
+    return (
+        <div>
+            <table className="table w-full">
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </table>
+        </div>
+    );
+};
+
+export default CustomerList;
