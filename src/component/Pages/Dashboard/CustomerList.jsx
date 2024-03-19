a5531671d650cbc1e1fd6ab775a66548623d6cec
sabrinara <saashid18@gmail.com>
2024-03-20 02:33:20 +0600

    customer list

--- src/component/Pages/Dashboard/CustomerList.jsx
+++ src/component/Pages/Dashboard/CustomerList.jsx
@@ -3,6 +3,7 @@
 const CustomerList = () => {
     return (
         <div>
+            <h1 className='text-4xl my-4 text-center md:mr-20 text-cyan-600 font-bold '>Customer List</h1>
             <table className="table w-full">
                 <th>No</th>
                 <th>Name</th>
