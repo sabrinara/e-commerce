d8d2370552ea5b73ec395f3e360d586439932114
sabrinara <saashid18@gmail.com>
2024-03-20 02:46:58 +0600

    login registration page design done

--- src/component/Pages/Login.jsx
+++ src/component/Pages/Login.jsx
@@ -1,9 +1,33 @@
+import { useNavigate } from "react-router-dom";
 
 
 const Login = () => {
+    const navigation = useNavigate();
+    navigation('/');
     return (
         <div>
-            this is login
+          <h1 className="text-4xl text-center font-bold my-10 text-cyan-600 ">Login</h1>
+            <form className="w-3/4 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="text" placeholder="email" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="text" placeholder="password" className="input input-bordered" />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control my-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+                
+            </form>
         </div>
     );
 };
--- src/component/Pages/Registration.jsx
+++ src/component/Pages/Registration.jsx
@@ -1,9 +1,50 @@
+import { useNavigate } from "react-router-dom";
 
 
 const Registration = () => {
+    const navigation = useNavigate();
+     navigation('/');
     return (
         <div>
-            this is registration
+            <h1 className='text-4xl text-center font-bold my-10 text-cyan-600 '>Registration</h1>
+
+            <form className="w-3/4 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" placeholder="name" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="text" placeholder="email" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Address</span>
+                    </label>
+                    <input type="text" placeholder="address" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Phone</span>
+                    </label>
+                    <input type="text" placeholder="phone" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="text" placeholder="password" className="input input-bordered" />
+                </div>
+
+                <div className="form-control my-6">
+                    <button className="btn btn-primary">Register</button>
+                </div>
+
+            </form>
         </div>
     );
 };
